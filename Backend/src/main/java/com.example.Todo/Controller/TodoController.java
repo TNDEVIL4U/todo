1e9a13622a4dd2b8d9bf574d7a8b30b9b7f40ad1
T S BHUVANESHWAR <bhuvaneshwarts1@gmail.com>
2025-12-07 19:29:21 +0530

    Update TodoController.java

--- Backend/src/main/java/com.example.Todo/Controller/TodoController.java
+++ Backend/src/main/java/com.example.Todo/Controller/TodoController.java
@@ -1,1 +1,55 @@
+package com.example.Todo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/todo")
+public class TodoController {
+    @Autowired
+    TodoService todoService;
+
+    @GetMapping("/{id}")
+    ResponseEntity<Todo> getTodo(@PathVariable Long id) {
+        try {
+            Todo todo = todoService.getTodoById(id);
+            return new ResponseEntity<>(todo,HttpStatus.OK);
+        }
+        catch (RuntimeException e)
+        {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+    @GetMapping
+    ResponseEntity<List<Todo>> getAllTodos() {
+        return new ResponseEntity<List<Todo>>(todoService.retrieveAllTodos(), HttpStatus.OK);
+    }
+
+    @PostMapping("/create")
+    ResponseEntity<Todo> createTodo(@RequestBody Todo todo)
+    {
+        return new ResponseEntity<>(todoService.createTodo(todo),HttpStatus.CREATED);
+    }
+
+    @PutMapping
+    public ResponseEntity<Todo> updateTodo(@RequestBody Todo todo)
+    {
+        return new ResponseEntity<>(todoService.updateTodo(todo),HttpStatus.OK);
+    }
+    @DeleteMapping("/delete/{id}")
+    void  deleteTodoById(@PathVariable Long id)
+    {
+        todoService.deleteTodoById(id);
+    }
+    @DeleteMapping
+    void deleteTodo()
+    {
+        todoService.deleteTodo();
+    }
+
+}
 
